8223b486278f535a1ba32a15cb0d22df22b6110f
Shriji-technoaspire <87520598+Shriji-technoaspire@users.noreply.github.com>
2022-04-15 11:49:16 +0530

    Add files via upload

---
+++ code/code.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTS(10, 11); // RX | TX
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("Enter AT commands:");
+  BTS.begin(38400);  // HC-05 FC-114 default speed in AT command more
+ }
+void loop()
+ {
+  // Read from HC-05 and send it to Arduino Serial Monitor
+  if (BTS.available())
+  {
+    Serial.write(BTS.read());
+  }
+  // Read from Arduino Serial Monitor and send to HC-05
+  if (Serial.available())
+  BTS.write(Serial.read());
+}
